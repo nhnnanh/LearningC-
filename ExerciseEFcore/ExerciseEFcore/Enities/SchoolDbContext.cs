213b7d021efcb06d7b83e6005b3de0f0d1a4d355
Ngoc Anh Nguyen Hoang <ngocanhk20.fptdn@gmail.com>
2026-05-23 22:15:14 +0700

    EFCore exercire

---
+++ ExerciseEFcore/ExerciseEFcore/Enities/SchoolDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace ExerciseEFcore.Enities;
+
+public partial class SchoolDbContext : DbContext
+{
+    public SchoolDbContext()
+    {
+    }
+
+    public SchoolDbContext(DbContextOptions<SchoolDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("DBDefault");
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC07EB5B39B5");
+
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ExerciseEFcore/ExerciseEFcore/Repository/StudentsRepo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExerciseEFcore.Enities;
+
+namespace ExerciseEFcore.Repository
+{
+    internal class StudentsRepo
+    {
+        SchoolDbContext context;
+        public StudentsRepo(SchoolDbContext context) { 
+            this.context = context;
+        }
+
+        public void Add(Student student) {
+            context.Students.Add(student);
+            context.SaveChanges();
+        }
+
+
+        public void Remove(int id)
+        {
+            var existingStudent = context.Students.FirstOrDefault(s => s.Id == id);
+            if (existingStudent != null)
+            {
+                context.Students.Remove(existingStudent);
+                context.SaveChanges();
+            }
+        }
+
+        public List<Student> GetAll() {
+            return context.Students.ToList();
+        }
+
+         public Student GetById(int id) {
+            return context.Students.FirstOrDefault(s => s.Id == id);
+         }
+         
+         public Student GetByName(string name) { return context.Students.FirstOrDefault(s => s.Name == name); }
+         
+         public void Update(Student student) {
+                var existingStudent = context.Students.FirstOrDefault(s => s.Id == student.Id);
+                if (existingStudent != null) {
+                    existingStudent.Name = student.Name;
+                    existingStudent.Age = student.Age;
+                    existingStudent.Email = student.Email;
+                    context.SaveChanges();
+                }
+        }
+
+
+
+
+
+    }
+}
